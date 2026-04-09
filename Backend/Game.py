417b776a15f87601ca43b429e7024fbe50b865ff
devbysamcloudy <snganga685@gmail.com>
2026-04-09 09:53:10 +0300

    THG-62 Create game class

---
+++ Backend/Game.py
@@ -0,0 +1,42 @@
+from Deck import Deck
+from Player import Player
+
+class Game():
+    def __init__(self):
+        self.main_pot = 0
+        self.current_pot = 0
+        deck = Deck()
+        deck.shuffle()
+        deck.shuffle()
+        human_cards= [deck.give_card(), deck.give_card()]
+        pc_cards = [deck.give_card(), deck.give_card()]
+
+        self.human =Player(type="human",
+                           cards = human_cards,
+                           total_amount_bet= 0,
+                           amount= 2000)
+        
+        self.pc = Player(type="pc",
+                         cards=pc_cards,
+                         total_amount_bet=0,
+                         name="Sam",
+                         amount=2000)
+        
+        self.deck = deck
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.deck.print_deck()
+    print("This is the deck")
+    print("Pc cards")
+    game.pc.cards[0].printCard()
+    game.pc.cards[1].printCard()
+    print("This is the deck")
+    print("Human cards")
+    game.human.cards[0].printCard()
+    game.human.cards[1].printCard()
+
+
+
+                                
