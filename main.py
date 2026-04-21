63a49251a972e1d764706bfaf7ef497aecfba770
devbysamcloudy <snganga685@gmail.com>
2026-04-21 10:07:30 +0300

    updated commit

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from flask import Flask, jsonify
 from prisma import Prisma
+# from app import create_app
+from flask import Blueprint
 
 
+
+
+# app = create_app()
+
+students_bp = Blueprint('students', __name__)
 
 
 app = Flask(__name__)
-@app.route('/students', methods=['GET'])
+
+@students_bp.route('/', methods=['GET'])
 async def list_Students():
-    prisma = Prisma()
-    await prisma.connect()
+    return jsonify({'message': 'List of students'})
+
+
+    # prisma = Prisma()
+    # await prisma.connect()
 
 
 
@@ -23,15 +34,41 @@ async def list_Students():
 
 
     
-    user = await prisma.users.find_first()
+    # user = await prisma.users.find_first()
 
-    student_dict = user.model_dump()
-    prisma.disconnect()
+    # student_dict = user.model_dump()
+    # prisma.disconnect()
+
+@students_bp.route('/add', methods=['POST'])
+async def add_Student():
+    return jsonify({'message': 'Student added successfully'})
+
+    # prisma = Prisma()
+    # await prisma.connect()
+    # user = await prisma.users.create(
+    #     data={
+    #         'name': 'John Doe',
+    #         'email': 'john.doe@example.com'
+    #     }
+    # )
+    # prisma.disconnect()
+
+    # return jsonify(user.model_dump()),201   
+
+@students_bp.route('/<int:student_id>', methods=['GET'])
+async def get_Student(student_id):
+    return jsonify({'message': f'Student with ID {student_id} retrieved successfully'})
+
+@students_bp.route('/del/<int:student_id>', methods=['DELETE'])
+async def delete_Student(student_id):
+    return jsonify({'message': f'Student with ID {student_id} deleted successfully'})
 
 
-    return jsonify(student_dict),200
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.register_blueprint(students_bp, url_prefix='/students')
+    app.run(debug=True, port=5000)
 
 
+    
